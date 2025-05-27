873707c4306d7caabbe990a8d46631de2b0ca2ac
rabin-creator <rabindc77@gmail.com>
2025-05-27 22:16:13 +0545

    second commit

---
+++ gh.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1;
+    while(i<20){
+    if (i%2==0)
+    {
+printf("%d \n",i);
+    }
+        i=i+1;
+    } 
+    return 0;                               // i=0   ohhhh i+1??? kata?
+}// terminal bata run kasari garne ?? ahhh   
