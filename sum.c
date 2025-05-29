b3ac96b4bdbce598231a60b72373b946911a68a0
rabin-creator <rabindc77@gmail.com>
2025-05-29 22:27:51 +0545

    fifth commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the two numbers:");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the sum of %d and %d is %d",a,b,sum);
+    return 0;
+    
+}
